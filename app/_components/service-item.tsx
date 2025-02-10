7185a633209046dd5c839a647a60ece8018a40b2
cajui54 <cajui54@gmail.com>
2025-02-10 12:28:29 -0300

    feat: ensure barbershop services are shown on barbershop page

---
+++ app/_components/service-item.tsx
@@ -0,0 +1,42 @@
+import { BarbershopService } from '@prisma/client'
+import Image from 'next/image'
+import React from 'react'
+import { formatCurrency } from '../_helpers/currency'
+import { Button } from './ui/button'
+import { Card, CardContent } from './ui/card'
+interface ServiceItemProps {
+  service: BarbershopService
+}
+
+const ServiceItem = ({ service }: ServiceItemProps) => {
+  return (
+    <Card>
+      <CardContent className="flex items-center gap-3 p-3">
+        <div className="relative max-h-[110px] min-h-[110px] min-w-[110px] max-w-[110px]">
+          <Image
+            alt={service.name}
+            src={service.imageURL}
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-sm font-semibold">{service.name}</h3>
+          <p className="text-sm text-gray-400">{service.description}</p>
+
+          <div className="flex items-center">
+            <p className="text-sm font-bold text-primary">
+              {formatCurrency(Number(service.price))}
+            </p>
+
+            <Button variant="secondary" className="ml-24" size="sm">
+              Reservar
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default ServiceItem
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from '@/app/_components/service-item'
 import { Button } from '@/app/_components/ui/button'
 import { db } from '@/app/_lib/prisma'
 import {
@@ -21,10 +22,15 @@ const BarbershopPage = async ({ params }: BarbershopPageParams) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   })
   if (!barbershop) {
     return notFound()
   }
+  console.log(barbershop.services)
+
   return (
     <div>
       <div className="relative h-[250px] w-full">
@@ -72,6 +78,15 @@ const BarbershopPage = async ({ params }: BarbershopPageParams) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h3 className="font-bold uppercase text-gray-400">Sobre nós</h3>
         <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
